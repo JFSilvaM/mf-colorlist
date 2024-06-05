3870aa0cdceba3c64af9e1392329eccf3b73639f
joaofsma <joaofsma@ext.inditex.com>
2024-06-05 10:13:28 +0200

    dev mode

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,10 @@ const Dotenv = require("dotenv-webpack");
 const deps = require("./package.json").dependencies;
 module.exports = (_, argv) => ({
   output: {
-    publicPath: "https://mfcolorlist.netlify.app/",
+    publicPath:
+      argv.mode === "development"
+        ? "http://localhost:3002"
+        : "https://mfcolorlist.netlify.app/",
   },
 
   resolve: {
